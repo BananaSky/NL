b948192bb5adc176dd4c01747f630db5dbd3a3de
LSaldyt <lucassaldyt@yahoo.com>
2017-01-26 10:15:50 -0700

    Run script no longer requires su

--- run.sh
+++ run.sh
@@ -4,6 +4,4 @@ cabal sandbox init
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cabal install -j
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-sudo cp .cabal-sandbox/bin/Riemann .
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-./Riemann
+./.cabal-sandbox/bin/Riemann
